9e4a03a5273a031a4e63616f3c3c1922469fb3c9
choseongwon <qqwow741@gmail.com>
2019-10-17 10:03:46 +0900

    using hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+//import ReducerSample from "./ReducerSample";
+//<ReducerSample/>
 //import Counter from "./Counter";
+//<Counter />
 import MyForm from "./MyForm";
+
 
 const App: React.FC = () => {
   const onSubmit = (form: { name: string; description: string }) => {
     console.log(form);
   };
-  return <MyForm onSubmit={onSubmit} />;
+  return (
+      <MyForm onSubmit={onSubmit} />
+  );
 }
 
 export default App;
--- src/MyForm.tsx
+++ src/MyForm.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 type MyFormProps = {
     onSubmit: (form: { name: string; description: string }) => void;
 };
 
 const MyForm = ({ onSubmit }: MyFormProps) => {
+    const inputRef = useRef<HTMLInputElement>(null);
     const [form, setForm] = useState({
         name: '',
         description: ''
@@ -27,6 +28,10 @@ const MyForm = ({ onSubmit }: MyFormProps) => {
             name: '',
             description: ''
         });
+        if(!inputRef.current){
+            return;
+        }
+        inputRef.current.focus();
     };
 
     return (
